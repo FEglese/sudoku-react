4119637b7b1448991d862b1f31a74194bcfd946a
FredrikEglese <fredrik.eglese@gmail.com>
2021-05-31 23:01:05 +0100

    boardValidation tests

--- src/services/boardValidationService.test.ts
+++ src/services/boardValidationService.test.ts
@@ -1,4 +1,20 @@
-import { isArrayValid } from "./boardValidationService";
+import {
+	isArrayValid,
+	getCol,
+	getSquareValues,
+} from "./boardValidationService";
+
+import { fullInvalidBoard } from "./testConstants";
+
+import Cell from "../interfaces/cell";
+
+const inputBoard1: Cell[][] = Array(9).fill(
+	Array(9).fill({ value: 1, isFixed: false })
+);
+
+const inputBoard2: Cell[][] = Array(9).fill(
+	Array(9).fill({ value: 4, isFixed: false })
+);
 
 describe("isArrayValid method", () => {
 	it("returns true if the array is valid", () => {
@@ -7,7 +23,64 @@ describe("isArrayValid method", () => {
 	});
 
 	it("returns false if the array is invalid", () => {
-		expect(isArrayValid([1, 4, 3, 4, 5, 6, 5, 8, 9])).toBe(false);
+		expect(isArrayValid([1, 4, -3, 4, 5, 6, 5, 8, 9])).toBe(false);
 		expect(isArrayValid([9, 2, 3, 4, 5, 3, 99, 8, 1])).toBe(false);
+	});
+});
+
+describe("getCol method", () => {
+	it("Returns the corect values in basic cases", () => {
+		expect(getCol(0, inputBoard1)).toMatchObject(Array(9).fill(1));
+		expect(getCol(3, inputBoard2)).toMatchObject(Array(9).fill(4));
+	});
+
+	it("Returns the corect values in complex cases", () => {
+		expect(getCol(0, fullInvalidBoard)).toMatchObject([
+			9, 5, 9, 7, 9, 5, 1, 7, 1,
+		]);
+
+		expect(getCol(2, fullInvalidBoard)).toMatchObject([
+			4, 2, 3, 8, 1, 1, 6, 5, 7,
+		]);
+	});
+});
+
+describe("getSquareValues validation", () => {
+	it("Returns empty for an input too short (0)", () => {
+		expect(() => {
+			getSquareValues(0, []);
+		}).toThrow("Invalid Params");
+	});
+
+	it("Returns empty for an input too long (10)", () => {
+		expect(() => {
+			getSquareValues(0, [[], [], [], [], [], [], [], [], [], []]);
+		}).toThrow("Invalid Params");
+	});
+
+	it("Throws error if square value is invalid", () => {
+		expect(() => {
+			getSquareValues(12, [[], [], [], [], [], [], [], [], []]);
+		}).toThrow("Invalid Params");
+
+		expect(() => {
+			getSquareValues(-3, [[], [], [], [], [], [], [], [], []]);
+		}).toThrow("Invalid Params");
+	});
+});
+
+describe("getSquareValues correct return values", () => {
+	it("returns the correct value from a valid input", () => {
+		expect(getSquareValues(0, fullInvalidBoard)).toMatchObject([
+			9, 1, 4, 5, 3, 2, 9, 1, 3,
+		]);
+
+		expect(getSquareValues(8, fullInvalidBoard)).toMatchObject([
+			2, 4, 7, 6, 8, 3, 2, 9, 6,
+		]);
+
+		expect(getSquareValues(2, fullInvalidBoard)).toMatchObject([
+			8, 2, 6, 1, 7, 9, 8, 6, 7,
+		]);
 	});
 });
--- src/services/boardValidationService.ts
+++ src/services/boardValidationService.ts
@@ -1,19 +1,42 @@
 import Cell from "../interfaces/cell";
 
+/**
+ * Gets the array of values in the 3x3 squares of the sudoku board
+ *
+ * Top left has index 0, to the right is 1 and so on down to 8 in the
+ * bottom right corner
+ *
+ * @param squareNum index of the 3x3 square from the board
+ * @param board input board to return the value of
+ * @returns array of values in that square. Empty array if invalid
+ */
 function getSquareValues(squareNum: number, board: Cell[][]): number[] {
-	let valuesArray: Cell[][] = [];
+	// Validation
+	if (board.length !== 9 || squareNum > 8 || squareNum < 0) {
+		throw new Error("Invalid Params");
+	}
+
+	let valuesArray: Cell[] = [];
 	let baseRow = Math.floor(squareNum / 3) * 3;
 	let baseCol = (squareNum % 3) * 3;
 
-	valuesArray.push(board[baseRow].slice(baseCol, baseCol + 3));
-	valuesArray.push(board[baseRow + 1].slice(baseCol, baseCol + 3));
-	valuesArray.push(board[baseRow + 2].slice(baseCol, baseCol + 3));
-	return valuesArray.flat(1).map((x) => x.value);
+	valuesArray.push(...board[baseRow].slice(baseCol, baseCol + 3));
+	valuesArray.push(...board[baseRow + 1].slice(baseCol, baseCol + 3));
+	valuesArray.push(...board[baseRow + 2].slice(baseCol, baseCol + 3));
+	return valuesArray.map((x) => x.value);
 }
 
-function isArrayValid(line: number[]): boolean {
+/**
+ * Returns whether the array contains all of the values from 1 to 9
+ * and is therefore a correct/valid series of integers
+ *
+ * @param inputArray The input array being validated against
+ * @returns true if the array is valid
+ */
+function isArrayValid(inputArray: number[]): boolean {
 	const correctSequence = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-	return JSON.stringify(line.sort()) === JSON.stringify(correctSequence);
+	if (inputArray.length !== 9) return false;
+	return JSON.stringify(inputArray.sort()) === JSON.stringify(correctSequence);
 }
 
 function completeStatus(board: Cell[][]): string {
@@ -47,4 +70,4 @@ function getCol(colNum: number, board: Cell[][]): number[] {
 	return collumn.map((x) => x.value);
 }
 
-export { getSquareValues, isArrayValid, completeStatus };
+export { getSquareValues, isArrayValid, completeStatus, getCol };
