c36b8551ba74f73787735863f4ecdbc70dcaaa27
FredrikEglese <fredrik.eglese@gmail.com>
2021-05-31 16:20:30 +0100

    Init boardvalidationservice refactor and tests

--- src/services/boardValidationService.test.ts
+++ src/services/boardValidationService.test.ts
@@ -1,7 +1,13 @@
-import {} from "./boardValidationService";
+import { isArrayValid } from "./boardValidationService";
 
-describe("It runs", () => {
-	it("returns true", () => {
-		expect(true).toBe(true);
+describe("isArrayValid method", () => {
+	it("returns true if the array is valid", () => {
+		expect(isArrayValid([1, 2, 3, 4, 5, 6, 7, 8, 9])).toBe(true);
+		expect(isArrayValid([9, 2, 6, 4, 5, 3, 7, 8, 1])).toBe(true);
+	});
+
+	it("returns false if the array is invalid", () => {
+		expect(isArrayValid([1, 4, 3, 4, 5, 6, 5, 8, 9])).toBe(false);
+		expect(isArrayValid([9, 2, 3, 4, 5, 3, 99, 8, 1])).toBe(false);
 	});
 });
--- src/services/boardValidationService.ts
+++ src/services/boardValidationService.ts
@@ -1,6 +1,6 @@
 import Cell from "../interfaces/cell";
 
-function getSquareValues(squareNum: number, board: Cell[][]): Cell[] {
+function getSquareValues(squareNum: number, board: Cell[][]): number[] {
 	let valuesArray: Cell[][] = [];
 	let baseRow = Math.floor(squareNum / 3) * 3;
 	let baseCol = (squareNum % 3) * 3;
@@ -8,23 +8,23 @@ function getSquareValues(squareNum: number, board: Cell[][]): Cell[] {
 	valuesArray.push(board[baseRow].slice(baseCol, baseCol + 3));
 	valuesArray.push(board[baseRow + 1].slice(baseCol, baseCol + 3));
 	valuesArray.push(board[baseRow + 2].slice(baseCol, baseCol + 3));
-	return valuesArray.flat(1);
+	return valuesArray.flat(1).map((x) => x.value);
 }
 
-function isArrayValid(line: Cell[]): boolean {
-	const correctSequence = "[1,2,3,4,5,6,7,8,9]";
-	return JSON.stringify(line.map((x) => x.value).sort()) === correctSequence;
+function isArrayValid(line: number[]): boolean {
+	const correctSequence = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	return JSON.stringify(line.sort()) === JSON.stringify(correctSequence);
 }
 
 function completeStatus(board: Cell[][]): string {
 	const flattenedCells = board.slice().flat(1);
 
-	if (flattenedCells.filter((cell) => cell.value === 0).length) {
-		return "Sudoku!";
+	for (let i = 0; i < flattenedCells.length; i++) {
+		if (!flattenedCells[i].value) return "Sudoku!";
 	}
 
 	for (var i = 0; i < 9; i++) {
-		var thisRow = board[i];
+		var thisRow = board[i].map((x) => x.value);
 		var thisCol = getCol(i, board);
 		var thisSquare = getSquareValues(i, board);
 
@@ -39,12 +39,12 @@ function completeStatus(board: Cell[][]): string {
 	return "Winner! Well done!";
 }
 
-function getCol(colNum: number, board: Cell[][]): Cell[] {
+function getCol(colNum: number, board: Cell[][]): number[] {
 	var collumn = [];
 	for (var i = 0; i < board.length; i++) {
 		collumn.push(board[i][colNum]);
 	}
-	return collumn;
+	return collumn.map((x) => x.value);
 }
 
 export { getSquareValues, isArrayValid, completeStatus };
